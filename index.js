c9fe86fcf62becc6cd10f5c444ad905eb713832a
dorothyas <dorotheeasiimwe@gmail.com>
2021-09-20 23:40:14 +0300

    modify files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const app = express();
+require('dotenv').config();
+
 
 app.get('/', (req, res) => {
     res.send('this is a demo app')
    
 });
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(process.env.PORT, () => console.log('listening on port 3000'));
